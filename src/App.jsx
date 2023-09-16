5208fe571820d769eb0037afc6a872176fa6d699
M.Anas Khan <a4anas406@gmail.com>
2023-09-16 21:22:06 +0500

    Password Generator

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import { useState } from 'react';
+import React from 'react';
+import './App.css'
+import { uppercaseLetters, lowercaseLetters, num, speachalCharacters } from './characters';
+
+function App() {
+
+  const [password, setPassword] = useState('');
+  const [passwordLength, setPasswordLength] = useState(20);
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const handleGeneratePassword = (e) => {
+    let charactersList = ""
+
+    if (uppercase) {
+      charactersList = charactersList + uppercaseLetters;
+    }
+
+    if (lowercase) {
+      charactersList = charactersList + lowercaseLetters;
+    }
+
+    if (numbers) {
+      charactersList = charactersList + num;
+    }
+
+    if (symbols) {
+      charactersList = charactersList + speachalCharacters;
+    }
+
+    setPassword(generatePassword(charactersList));
+  }
+
+  const generatePassword = (charactersList) => {
+    let password = '';
+    const charactersListLength = charactersList.length;
+
+    for (let i = 0; i < passwordLength; i++){
+      const charactersIndex = Math.round(Math.random() * charactersListLength);
+      
+      password = password + charactersList.charAt(charactersIndex)
+    }
+
+    return password
+  }
+
+  const copyPassword = () => {
+    const newTextArea =document.createElement("textarea")
+    newTextArea.innerText = password
+    document.body.appendChild(newTextArea)
+    newTextArea.select()
+    document.execCommand("copy")
+    newTextArea.remove()
+  }
+
+  const handleCopyBtn = (e) => {
+    copyPassword()
+  }
+
+
+
+
+  return (
+    <div className="App">
+
+      <div className="contaner">
+
+        <div className="generator">
+
+          <h1><i class="fa-solid fa-lock"></i></h1>
+
+          <h2 className="app-name">Password Generator</h2>
+
+          <div className="generate-password-div">
+            <h3>{password}</h3>
+            <button onClick={handleCopyBtn} className="btn-copy"><i class="fa-solid fa-copy"></i></button>
+          </div>
+
+          <div className='form'>
+            <label htmlFor="password">Password Length</label>
+            <input
+              defaultValue={passwordLength}
+              onClick={(e) => setPasswordLength(e.target.value)}
+              type="number" name="password" id="password" min={8} max={20} />
+          </div>
+
+          {/* ******************** Uppercase Letters ********************** */}
+          <div className='form'>
+            <label htmlFor="Uppercase">Uppercase Letters</label>
+            <input
+              checked={uppercase}
+              onClick={(e) => setUppercase(e.target.checked)}
+              type='checkbox' name="Uppercase" id="Uppercase"
+            />
+          </div>
+
+          {/* ******************** Lowercase Letters ********************** */}
+          <div className='form'>
+            <label htmlFor="Lowercase">Lowercase Letters</label>
+            <input
+              checked={lowercase}
+              onClick={(e) => setLowercase(e.target.checked)}
+              type='checkbox' name="Lowercase" id="Lowercase"
+            />
+          </div>
+
+          {/* ********************** Numbers ****************************** */}
+          <div className='form'>
+            <label htmlFor="Numbers">Numbers</label>
+            <input
+              checked={numbers}
+              onClick={(e) => setNumbers(e.target.checked)}
+              type='checkbox' name="Numbers" id="Numbers"
+            />
+          </div>
+
+          {/* ************************ Symbols **************************** */}
+          <div className='form'>
+            <label htmlFor="Symbols">Symbols</label>
+            <input
+              checked={symbols}
+              onClick={(e) => setSymbols(e.target.checked)}
+              type='checkbox' name="Symbols" id="Symbols"
+            />
+          </div>
+
+          <button onClick={handleGeneratePassword} className='generate-password'> Generate Password</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/characters.js
@@ -0,0 +1,4 @@
+export const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+export const num = "0123456789";
+export const speachalCharacters = "!@#$%&*()[]{}<>/\|-+=:;',.?_~";
